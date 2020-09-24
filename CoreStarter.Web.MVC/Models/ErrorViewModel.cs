7f87e798e380b9cd83e5315c933b8d5ca8f71d17
ahmedlotfiali <ahmedlotfiali@gmail.com>
2020-09-24 11:27:44 +0200

    init CoreStarter.Web.MVC

---
+++ CoreStarter.Web.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreStarter.Web.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
